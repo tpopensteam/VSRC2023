12163681e97bee982bcaa4ffd32910935cda469f
Phạm Xuân Đạt <136553197+PXDAT@users.noreply.github.com>
2023-09-20 11:51:18 +0700

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,39 +2,23 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <PS2X_lib.h>
-  
-
+#include <cstdlib>
 #define PS2_DAT 12
 #define PS2_CMD 13
 #define PS2_SEL 15
 #define PS2_CLK 14
-#define SERVO 2
-#define SERVO2 3
-#define SERVO3 4
-#define SERVO4 5
-#define SERVO_FREQ_MIN 200
-#define SERVO_FREQ_MAX 550
-#define SERVO_MIN_DEGREE 0
-#define SERVO_MAX_DEGREE 180
-
 #define ENA
 #define ENB 
-
 #define pressures false
 #define rumble false
-
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 PS2X ps2x;
-bool mode_drive = false;
-int servo1 = 0, servo2 = 0, servo3 = 0, servo4 = 0, collector_mode = 0, shooter_mode = 0;
+bool shooter_value = false, servo_intake = false;
 void setup() 
 {
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
-  pwm.setPWMFreq(50);
-
-  pwm.setPWM(SERVO,0,0);
-
+  pwm.setPWMFreq(60);
   Wire.setClock(400000);
   Serial.begin(115200);
   Serial.print("Ket noi voi tay cam PS2)");
@@ -64,280 +48,122 @@ void setup()
 
 }
 // servo setup
-void servo_clockwise(uint8_t Servo)
+void servo_clockwise(uint16_t Servo)
 {
-  pwm.setPWM(Servo,0,180);
+  pwm.setPWM(Servo, 0, 180);
 }
-void servo_anticlockwise(uint8_t Servo1)
+void servo_anticlockwise(uint16_t Servo1)
 {
-  pwm.setPWM(Servo1, 90, 680);
+  pwm.setPWM(Servo1, 90, 580);
+}
+void servo_clockwise180(uint16_t Servo3)
+{
+  pwm.setPWM(Servo3, 0, 512);
+}
+void servo_anticlockwise180(uint16_t Servo4)
+{
+  pwm.setPWM(Servo4, 0, 205);
 }
 void stop_servo(uint8_t Servo2)
 {
   pwm.setPWM(Servo2, 0, 0);
 }
 // motor setup
-void forwarddc(uint16_t forward, uint16_t forward2)
-{
-  pwm.setPWM(10,0,forward);
-  pwm.setPWM(11,0,forward2);
-}
-void forward2dc(uint16_t fur, uint16_t fur2)
-{
-  pwm.setPWM(12,0,fur);
-  pwm.setPWM(13,0,fur2);
-}
 void rightdc(uint16_t right, uint16_t right2)
 {
-  pwm.setPWM(10,0,right);
-  pwm.setPWM(11,0,right2);
+  pwm.setPWM(12, 0, right);
+  pwm.setPWM(13, 0, right2);
 }
-void right2dc(uint16_t right0, uint16_t right1)
+void middle(uint16_t middle1, uint16_t middle2)
 {
-  pwm.setPWM(12,0,right0);
-  pwm.setPWM(13,0,right1);
+  pwm.setPWM(14, 0, middle1);
+  pwm.setPWM(15, 0, middle2);
 }
-void leftdc(uint16_t left, uint16_t left2)
+void leftdc(uint16_t left0, uint16_t left1)
 {
-  pwm.setPWM(10,0,left);
-  pwm.setPWM(11,0,left2);
-}
-void left2dc(uint16_t left0, uint16_t left1)
-{
-  pwm.setPWM(12,0,left0);
-  pwm.setPWM(13,0,left1);
-}
-void backdc(uint16_t back, uint16_t back2)
-{
-  pwm.setPWM(10,0,back);
-  pwm.setPWM(11,0,back2);
-}
-void back2dc(uint16_t back0, uint16_t back1)
-{
-  pwm.setPWM(12,0,back0);
-  pwm.setPWM(13,0,back1);
-}
-// motor mode speed
-void eco()
-{
-  if(ps2x.Analog(PSS_RY) == 0 && ps2x.Analog(PSS_LY) == 0)
-  {
-    forwarddc(0,1000);
-    forward2dc(0,1000);
-  }
-  else if(ps2x.Analog(PSS_RY) == 255 && ps2x.Analog(PSS_LY) == 255)
-  {
-    backdc(1000,0);
-    back2dc(1000,0);
-  }
-  else if(ps2x.Analog(PSS_RY) == 0 && ps2x.Analog(PSS_LY) == 255)
-  {
-    left2dc(0,1000);
-    leftdc(1000,0);
-  } 
-  else if(ps2x.Analog(PSS_RY) == 255 && ps2x.Analog(PSS_LY) == 0)
-  {
-    rightdc(0,1000);
-    right2dc(1000,0);
-  }
-  else if(ps2x.Analog(PSS_LY) == 0)
-  {
-    forwarddc(0,1000);
-  }
-  else if(ps2x.Analog(PSS_RY) == 0)
-  {
-    forward2dc(0,1000);
-  }
-  else if(ps2x.Analog(PSS_LY) == 255)
-  {
-    backdc(1000,0);
-  }
-  else if(ps2x.Analog(PSS_RY) == 255)
-  {
-    back2dc(1000,0);
-  }
-  else
-  {
-    forwarddc(0,0);
-    forward2dc(0,0);
-    backdc(0,0);
-    back2dc(0,0);
-  }
-}
-void sport()
-{
-  if(ps2x.Analog(PSS_RY) == 0 && ps2x.Analog(PSS_LY) == 0)
-  {
-    forwarddc(0,3000);
-    forward2dc(0,3000);
-  }
-  else if(ps2x.Analog(PSS_RY) == 255 && ps2x.Analog(PSS_LY) == 255)
-  {
-    backdc(3000,0);
-    back2dc(3000,0);
-  }
-  else if(ps2x.Analog(PSS_RY) == 0 && ps2x.Analog(PSS_LY) == 255)
-  {
-    left2dc(0,3000);
-    leftdc(3000,0);
-  } 
-  else if(ps2x.Analog(PSS_RY) == 255 && ps2x.Analog(PSS_LY) == 0)
-  {
-    rightdc(0,3000);
-    right2dc(3000,0);
-  }
-  else if(ps2x.Analog(PSS_LY) == 0)
-  {
-    forwarddc(0,3000);
-  }
-  else if(ps2x.Analog(PSS_RY) == 0)
-  {
-    forward2dc(0,3000);
-  }
-  else if(ps2x.Analog(PSS_LY) == 255)
-  {
-    backdc(3000,0);
-  }
-  else if(ps2x.Analog(PSS_RY) == 255)
-  {
-    back2dc(3000,0);
-  }
-  else
-  {
-    forwarddc(0,0);
-    forward2dc(0,0);
-    backdc(0,0);
-    back2dc(0,0);
-  }
+  pwm.setPWM(10,0,left0);
+  pwm.setPWM(11,0,left1);
 }
 // shooter and collector setup
-void collector()
-{
-  pwm.setPWM(8,0,0);
-  pwm.setPWM(9,0,3000);
-}
-void reverse_collector()
-{
-  pwm.setPWM(8,0,3000);
-  pwm.setPWM(9,0,0);
-}
-void collector_stop()
-{
-  pwm.setPWM(8,0,0);
-  pwm.setPWM(9,0,0);
-}
 void shooter()
 {
-  pwm.setPWM(14,0,0);
-  pwm.setPWM(15,0,4000);
+  pwm.setPWM(8,0,0);
+  pwm.setPWM(9,0,4000);
 }
 void shooter_stop()
 {
-  pwm.setPWM(14,0,0);
-  pwm.setPWM(15,0,0);
+  pwm.setPWM(8,0,0);
+  pwm.setPWM(9,0,0);
 }
 // control setup
 void ps2Control() 
 {
   ps2x.read_gamepad(false, false);
-// motor mode controller
-  if(ps2x.ButtonPressed(PSB_TRIANGLE))
+  int joyleft = ps2x.Analog(PSS_LY);
+  int joyright = ps2x.Analog(PSS_RY);
+  int njoyl = ps2x.Analog(PSS_LX);
+  int njoyr = ps2x.Analog(PSS_RX);
+  njoyr = map(njoyr,0, 255, 4095, -4095);
+  njoyl = map(njoyl,0, 255, 4095, -4095);
+  joyright = map(joyright,0, 255, 4095, -4095);
+  joyleft = map(joyleft, 0, 255, 4095, -4095);
+  
+  if(joyleft > 17)
   {
-    mode_drive =!mode_drive;
+    leftdc(0, 0+joyleft);
+    Serial.print("joyleft: ");
+    Serial.println(joyleft);
   }
-  if(mode_drive)
+  else if(joyleft < 17)
   {
-    eco();
+    leftdc(0 + abs(joyleft), 0);
+    Serial.print("joyleft: ");
+    Serial.println(joyleft);
   }
   else
   {
-    sport();
+    leftdc(0,0);
   }
-//control servo 360 clockwise and anticlockwise 
-  if(ps2x.ButtonPressed(PSB_PAD_UP))
+  if(joyright > 17)
   {
-    servo1++;
+    rightdc(0, 0+joyright);
+    Serial.print("joyright: ");
+    Serial.println(joyright);
   }
-  switch(servo1 % 3)
+  else if(joyright < 17)
   {
-    case (0):
-    stop_servo(SERVO);
-    stop_servo(SERVO2);
-    break;
-    case(1):
-    servo_clockwise(SERVO);
-    servo_clockwise(SERVO2);
-    break;
-    case(2):
-    servo_anticlockwise(SERVO);
-    servo_anticlockwise(SERVO2);
-    break;
+    rightdc(0 + abs(joyright), 0);
+    Serial.print("joyright: ");
+    Serial.println(joyright);
   }
-// control servo 180 clockwise and anticlockwise
-  if(ps2x.ButtonPressed(PSB_PAD_LEFT))
+  else 
   {
-    servo3++;
+    rightdc(0,0);
   }
-  switch(servo3 % 3)
+  if(njoyl < 17 && njoyr < 17)
   {
-    case (0):
-    stop_servo(SERVO3);
-    break;
-    case(1):
-    servo_clockwise(SERVO3);
-    break;
-    case(2):
-    servo_anticlockwise(SERVO3);
-    break;
+    middle(0, 0 + abs(njoyl+njoyr) /2;
   }
-// control servo 180 (2) clockwise and anticlockwise
-  if(ps2x.ButtonPressed(PSB_PAD_RIGHT))
-  {
-    servo4++;
+  else if(njoyl > 17 &&  njoyr > 17)
+  {    
+    middle(0 + (njoyr + njoyl) / 2, 0);
   }
-  switch(servo4 % 3)
+  else
   {
-    case (0):
-    stop_servo(SERVO4);
-    break;
-    case(1):
-    servo_clockwise(SERVO4);
-    break;
-    case(2):
-    servo_anticlockwise(SERVO4);
-    break;
-  }
-//shooting motor
-  if(ps2x.ButtonPressed(PSB_R1))
-  {
-    shooter_mode++;
-  }
-  switch(shooter_mode % 2)
-  {
-    case(0):
-      shooter_stop();
-      break;
-    case(1):
-      shooter();
-      break;
-  }
-//ball collector motor
-  if(ps2x.Button(PSB_L2))
-  {
-    reverse_collector();
+    middle(0,0);
   }
   if(ps2x.ButtonPressed(PSB_L1))
   {
-    collector_mode++;
+    shooter_value = !shooter_value;
   }
-  switch(collector_mode % 2)
+  if(shooter_value)
   {
-    case (0):
-      collector_stop();
-      break;
-    case(1):
-      collector();
+    shooter();
   }
+  else
+  {
+    shooter_stop();
+  } 
   delay(50);
 }
 void loop() {ps2Control();}
